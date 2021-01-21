d0c5865696b407d5b0af4839142ad75946dc000f
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2021-01-21 15:10:54 +0400

    Config with secret keys in another file

---
+++ config_secret_keys.SAMPLE.py
@@ -0,0 +1,8 @@
+#Содержит секретные ключи, которые импортируются внутрь структуры конфигурационного массива
+config_secret_keys = {
+    'yandex':
+    {
+        "folderId": "b2g0448cl74fd31nesbf",
+        "Api-Key": "AQVN4D4Fw_mQt6U31gqPdj9P0pqnkKU0heEOA7uP",
+    },
+}
